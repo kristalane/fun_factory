0fd4a0cc01c636e978c6feb18cfe199fa7a50f7a
Krista Lane <kristalane@gmail.com>
2017-09-29 21:58:13 -0700

    added helper text to know when dummy pages have loaded to test initial routes

--- server.js
+++ server.js
@@ -3,19 +3,25 @@
 var express = require("express");
 var mongoose = require("mongoose");
 var bodyParser = require("body-parser");
+var logger = require("morgan");
+
+var PreferencesSchema = require("./server/model");
 
 //and create our instances
 var app = express();
-var router = express.Router();
 
 //set our port to either a predetermined port number, or 3001
 var port = process.env.API_PORT || 3001;
 
 //now we should configure the API to use bodyParser and look for
 //JSON data in the request body
-
-app.use(bodyParser.urlencoded({ extended: true }));
+app.use(logger("dev"));
 app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.text());
+app.use(bodyParser.json({ type: "application/vnd.api+json" }));
+
+app.use(express.static("public"));
 
 //To prevent errors from Cross Origin Resource Sharing, we will set
 //our headers to allow CORS with middleware like so:
@@ -28,13 +34,31 @@ app.use(function(req, res, next) {
  res.setHeader("Cache-Control", "no-cache");
  next();
 });
-//now we can set the route path & initialize the API
-router.get("/", function(req, res) {
- res.json({ message: "API Initialized!"});
-});
-//Use our router configuration when we call /api
-app.use("/api", router);
-//starts the server and listens for requests
+
+
+// // MongoDB Configuration configuration
+// mongoose.connect("#");
+// var db = mongoose.connection;
+//
+// db.on("error", function(err) {
+//   console.log("Mongoose Error: ", err);
+// });
+//
+// db.once("open", function() {
+//   console.log("Mongoose connection successful.");
+// });
+
+
+// //now we can set the route path & initialize the API
+// app.get("/", function(req, res) {
+//  res.json({ message: "API Initialized!"});
+// });
+//
 app.listen(port, function() {
  console.log("api running on port " + port);
+});
+
+// Any non API GET routes will be directed to our React App and handled by React Router
+app.get("*", function(req, res) {
+  res.sendFile(__dirname + "/public/index.html");
 });
