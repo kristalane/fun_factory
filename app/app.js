1dcfc29e1c0c26faad4a6fd6019b8a1e49fdfa1a
Krista Lane <kristalane@gmail.com>
2017-10-06 00:23:29 -0700

    more potentially unhelpful changes back to es5

--- app/app.js
+++ app/app.js
@@ -5,6 +5,7 @@ import ReactDOM from "react-dom";
 // Grabs the Routes
 var routes = require("./config/routes");
 
+
 // Renders the contents according to the route page
 // Displays the contents in the div app of index.html
 // Note how ReactDOM takes in two parameters (the contents and the location)
--- app/components/Main.js
+++ app/components/Main.js
@@ -1,38 +1,45 @@
+
 // Include React as a dependency
-import React from "react";
-
-// Create the Search component
-var Fun = React.createClass({
-
-  // Here we set the initial state variables
-  // (this allows us to propagate the variables for maniuplation by the children components
-  // Also note the "resuls" state. This will be where we hold the data from our results
-  getInitialState: function() {
-    return {
-      results: {}
-    };
-  },
-
-  // This function will be passed down into child components so they can change the "parent"
-  // i.e we will pass this method to the query component that way it can change the main component
-  // to perform a new search
-  // setQuery: function(newQuery, newStart, newEnd) {
-  //   helpers.runQuery(newQuery, newStart, newEnd).then(function(data) {
-  //     this.setState({ results: { docs: data.docs } });
-  //   }.bind(this));
-  // },
-
-  // Render the component. Note how we deploy both the Query and the Results Components
+var React = require("react");
+// Including the Link component from React Router to navigate within our application without full page reloads
+// https://github.com/ReactTraining/react-router/blob/master/docs/API.md#link
+var Link = require("react-router").Link;
+// Create the Main component
+var Main = React.createClass({
   render: function() {
-    console.log("Render Results", this.state.results);
-
     return (
+      // We can only render a single div. So we need to group everything inside of this main-container one
       <div className="main-container">
-          Hey this returned a thing
+        <div className="container">
+          {/* Navbar */}
+          <nav className="navbar navbar-default" role="navigation">
+            <div className="container-fluid">
+              <div className="navbar-header">
+                <Link className="navbar-brand" to="/">Fun FACTory</Link>
+              </div>
+              <div className="collapse navbar-collapse navbar-ex1-collapse">
+              </div>
+            </div>
+          </nav>
+          {/* Jumbotron */}
+          <div className="jumbotron">
+            <h2 className="text-center"><strong>(ReactJS) Fun Fact Shop</strong></h2>
+            <h3 className="text-center">Search for and save fun facts of interest.</h3>
+          </div>
+          {/* Here we will deploy the sub components (Search or Saved */}
+          {/* These sub-components are getting passed as this.props.children */}
+          {this.props.children}
+          <footer>
+            <hr />
+            <p className="pull-right">
+              <i className="fa fa-github" aria-hidden="true"></i>
+              Proudly built using React.js
+            </p>
+          </footer>
+        </div>
       </div>
     );
   }
 });
-
 // Export the module back to the route
-module.exports = Fun;
+module.exports = Main;
