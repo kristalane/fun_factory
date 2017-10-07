4b0c7b8f64cee4ff42af2cf455320765190d4a1e
Krista Lane <kristalane@gmail.com>
2017-10-07 00:16:42 -0700

    adding numbersAPI call to backend

--- app/components/Main.js
+++ app/components/Main.js
@@ -4,6 +4,7 @@ var React = require("react");
 // Including the Link component from React Router to navigate within our application without full page reloads
 // https://github.com/ReactTraining/react-router/blob/master/docs/API.md#link
 var Link = require("react-router").Link;
+var helpers = require("../utils/helpers");
 // Create the Main component
 var Main = React.createClass({
   render: function() {
